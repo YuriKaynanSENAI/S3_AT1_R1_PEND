3727276b7efafcffdf0f566f2f739bfbf7f9e4a4
Yuri_Puyu <yurikaynandesouzanogueira@gmail.com>
2025-04-04 10:58:30 -0300

    Comentarios add

--- Atividade_entrega/script/script.js
+++ Atividade_entrega/script/script.js
@@ -1,13 +1,15 @@
-console.log("JS CONECTADO!");
+console.log("JS CONECTADO!"); // Apenas um log para verificar se o script está rodando corretamente
 
+// Classe base Funcionario
 class Funcionario {
   constructor(nome, salario, dataAdmissao) {
-    this.nome = nome;
-    this.salario = salario;
-    this.dataAdmissao = dataAdmissao;
-    this.bonus = (salario * 10) / 100;
+    this.nome = nome; // Nome do funcionário
+    this.salario = salario; // Salário do funcionário
+    this.dataAdmissao = dataAdmissao; // Data de admissão do funcionário
+    this.bonus = (salario * 10) / 100; // Calcula o bônus (10% do salário)
   }
 
+  // Método para exibir o valor do bônus
   calcularBonus() {
     console.log(
       `${this.nome} terá um bônus de 10% equivalente a R$${this.bonus.toFixed(
@@ -16,6 +18,7 @@ class Funcionario {
     );
   }
 
+  // Método para exibir os detalhes do funcionário
   exibirDetalhes() {
     console.log(
       `Funcionário: ${this.nome}, salário: R$${this.salario.toFixed(
@@ -27,13 +30,15 @@ class Funcionario {
   }
 }
 
+// Classe Gerente que herda de Funcionario
 class Gerente extends Funcionario {
   constructor(nome, salario, dataAdmissao, departamento) {
-    super(nome, salario, dataAdmissao);
-    this.departamento = departamento;
-    this.bonus = (salario * 20) / 100;
+    super(nome, salario, dataAdmissao); // Chama o construtor da classe base
+    this.departamento = departamento; // Adiciona um novo atributo: departamento
+    this.bonus = (salario * 20) / 100; // Gerentes recebem 20% de bônus em vez de 10%
   }
 
+  // Sobrescrevendo o método calcularBonus para exibir a nova porcentagem
   calcularBonus() {
     console.log(
       `O gerente ${
@@ -42,6 +47,7 @@ class Gerente extends Funcionario {
     );
   }
 
+  // Sobrescrevendo o método exibirDetalhes para incluir o departamento
   exibirDetalhes() {
     console.log(
       `Gerente: ${this.nome}, salário: R$${this.salario.toFixed(
@@ -52,10 +58,13 @@ class Gerente extends Funcionario {
     );
   }
 }
-const gerente = new Gerente("Alcindo", 1000, "01/04/2025", "TI");
-gerente.calcularBonus();
-gerente.exibirDetalhes();
 
+// Criando um objeto Gerente e chamando seus métodos
+const gerente = new Gerente("Alcindo", 1000, "01/04/2025", "TI");
+gerente.calcularBonus(); // Exibe o bônus do gerente
+gerente.exibirDetalhes(); // Exibe os detalhes do gerente
+
+// Criando um objeto Funcionario e chamando seus métodos
 const funcionario = new Funcionario("Yuri", 1000, "01/04/2025");
-funcionario.calcularBonus();
-funcionario.exibirDetalhes();
+funcionario.calcularBonus(); // Exibe o bônus do funcionário
+funcionario.exibirDetalhes(); // Exibe os detalhes do funcionário
